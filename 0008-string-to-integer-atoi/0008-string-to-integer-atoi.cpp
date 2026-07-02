0b949bee1ef8a539b683206e225f8b1f5dbe18cf
Vansh Sharma <sharma.vansh1980@gmail.com>
2026-07-02 16:03:22 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.82%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    const int INT_MIN_VAL = -2147483648;
+    const int INT_MAX_VAL = 2147483647;
+    int helper(const string &s, int i, long long num, int sign) {
+        if (i >= s.size() || !isdigit(s[i])) return (int)(sign * num);
+        num = num * 10 + (s[i] - '0');
+        if (sign * num <= INT_MIN_VAL) return INT_MIN_VAL;
+        if (sign * num >= INT_MAX_VAL) return INT_MAX_VAL;
+
+    // Recurse for next character
+        return helper(s, i + 1, num, sign);
+    }
+    int myAtoi(string s,int i=0) {
+        while (i < s.size() && s[i] == ' ') i++;
+        int sign = 1;
+        if (i < s.size() && (s[i] == '+' || s[i] == '-')) {
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+
+    // Call recursive helper
+        return helper(s, i, 0, sign);
+    }
+};
