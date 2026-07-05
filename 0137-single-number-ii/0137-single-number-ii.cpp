cfc2a095dd392747ba25704c3c1fd6e30a944f1a
Vansh Sharma <sharma.vansh1980@gmail.com>
2026-07-06 02:56:09 +0530

    Time: 3 ms (30.88%), Space: 13.3 MB (86.87%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int,int> mpp;
-        for (int i=0;i<nums.size();i++){
-            mpp[nums[i]]++;
-        }
-        for (auto it:mpp){
-            if(it.second==1){
-                return it.first;
+        int ans=0;
+        for (int i=0;i<32;i++){
+            int cnt=0;
+            for (int j=0;j<nums.size();j++){
+                if(nums[j]&(1<<i)) cnt++;
             }
+            if(cnt%3==1) ans=ans|(1<<i);
         }
-        return -1;
+        return ans;
     }
 };
