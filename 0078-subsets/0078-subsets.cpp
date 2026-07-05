700f2b6c75a2fb5477aee899e641b815b65a07fe
Vansh Sharma <sharma.vansh1980@gmail.com>
2026-07-06 02:30:03 +0530

    Time: 2 ms (38.04%), Space: 9.8 MB (96.32%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     
-    void f(int index,int sum,vector<int> &arr,vector<vector<int>> &sums,vector<int> &ds){
-        if(index==arr.size()){
-            sums.push_back(ds);
-            return;
-        }
-        ds.push_back(arr[index]);
-        f(index+1,sum-arr[index],arr,sums,ds);
-        ds.pop_back();
-        f(index+1,sum,arr,sums,ds);
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        f(0,0,nums,ans,ds);
+        int n=nums.size();
+        int subsets=1<<n;
+        for(int i=0;i<subsets;i++){
+            vector<int> l;
+            for (int j=0;j<n;j++){
+                if(i&(1<<j)){
+                    l.push_back(nums[j]);
+                }
+            }
+            ans.push_back(l);
+        }
         sort(ans.begin(),ans.end());
         return ans;
     }
